43be6146e9fa5a7e29acee4ccfaaea047efbe8da
besh5 <sdvova555hp@gmail.com>
2025-12-30 10:48:14 -0600

    Mistake class test changes

--- src/volodymyrBesh/NewTestClass.java
+++ src/volodymyrBesh/NewTestClass.java
@@ -2,12 +2,12 @@ package volodymyrBesh;
 
 public class NewTestClass {
 
-    public class NewTestClass1 {
+
 
         public static void main(String[] args) {
             System.out.println("MIstake");
         }
 
-    }
+
 
 }
